4ed69d82695c043390ed5e34e578a5f94b875b0a
Yusuf olatunji  adeshina <72082053+YeezyWest@users.noreply.github.com>
2023-01-22 12:59:18 +0100

    update

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,38 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+
+const updateUI = (data) => {
+
+    const cityDets = data.cityDets;
+    const weather = data.weather;
+
+    //upadate details template
+    details.innerHTML = `
+            <h5 class="text-4xl uppercase  font-semibold mb-3">${cityDets.EnglishName}</h5>
+            <div class="text-lg uppercase  font-semibold text-gray-300 mb-6">${weather.WeatherText}</div>
+            <div class="my-3">
+            <span class="uppercase text-5xl mb-6 ">${weather.Temperature.Metric.Value}</span>
+            <span class="text-5xl font-thin mb-6">&deg;C</span>
+            </div>
+    `;
+
+    //remove class hidden if present
+    if (card.classList.contains('hidden')) {
+        card.classList.remove('hidden');
+    }
+};
 
 const updateCity = async (city) => {
 
-   const cityDets = await getCity(city);
-   const weather = await getWeather(cityDets.Key);
+    const cityDets = await getCity(city);
+    const weather = await getWeather(cityDets.Key);
 
-   return {
-    cityDets: cityDets,
-    weather: weather
-   };
+    return {
+        cityDets: cityDets,
+        weather: weather
+    };
 
 };
 
@@ -22,6 +46,6 @@ cityForm.addEventListener('submit', e => {
 
     //update the ul with new city 
     updateCity(city)
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
+        .then(data => updateUI(data))
+        .catch(err => console.log(err));
 });
